660a1a5149112a41682042b024904ccd4a2973f7
Larissa Silveira <larissa.silveira02@gmail.com>
2020-06-01 18:28:01 -0300

    Laços de repeticao for e while

---
+++ src/aula_4.py
@@ -0,0 +1,26 @@
+nota = int(input('Entre com a nota: '))
+while nota > 10:
+    nota =int(input('Nota invalida. Entre com a nota:' ))
+
+
+# a = 0
+# while a < 10:
+#     print(a)
+#     a+=1
+#
+#
+
+
+#laco de repeticao for
+# a = int(input('Digite um umero'))
+# for i in range(a+1):
+#     div = 0
+#     for x in range (1, i+1):
+#       resto = i % x
+#       print(x, resto)
+#       if resto == 0 :
+#           div += 1
+#
+#     if div == 2:
+#         print(i)
+#
