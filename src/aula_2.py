62cb87e9bb0c173f3b40f3cf2d2d7ae8014a24bd
Larissa Silveira <larissa.silveira02@gmail.com>
2020-06-01 18:25:35 -0300

    operadores aritmeticos

---
+++ src/aula_2.py
@@ -0,0 +1,14 @@
+a = int(input('Escreva um numero: '))
+b = int(input('Escreva outro numero: '))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+print("Soma: " + str(soma))
+print("Subtração: " + str(subtracao))
+print("Multiplicação: " + str(multiplicacao))
+print("Divisão: " + str(divisao))
+print("Resto da divisão: " + str(resto))
