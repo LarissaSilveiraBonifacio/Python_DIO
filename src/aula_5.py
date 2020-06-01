991cd086885fd6721e0c1956aaecc88512be4cd1
Larissa Silveira <larissa.silveira02@gmail.com>
2020-06-01 19:11:21 -0300

    Listas e tuplas

---
+++ src/aula_5.py
@@ -0,0 +1,21 @@
+lista = [1,3,5,7]
+lista_animais =['cachorro','gato','elefante']
+print(lista_animais[1])
+print(sum(lista))
+print(max(lista))
+print(min(lista))
+#criar mais um animal
+lista_animais.append('lobo')
+print(lista_animais)
+
+lista_animais.pop()
+print(lista_animais)
+#ordenar
+lista_animais.sort()
+print(lista_animais)
+#inverter
+lista_animais.reverse()
+print(lista_animais)
+#tupla
+tupla = (1,10,12,14)
+print(len(tupla))
